d8e02583b8cc9f504559593e232debf4672bf337
Soren Zaiser <soren.zaiser@gmail.com>
2022-03-02 10:19:48 -0500

    add comments

--- haskel_lab_3/addInts.hs
+++ haskel_lab_3/addInts.hs
@@ -1,25 +1,28 @@
 main :: IO ()
 main =
-  putStrLn "How many numbers would you like to add? "
-    >>  readInt
-    >>= \count -> addFromConsole count
-    >>= \output -> 
-        putStrLn "Your Sum: "
-        >> print output
+  putStrLn "How many numbers would you like to add? " -- Print initial prompt
+    >>  readInt                             -- Read Initial Int from Console to determine count
+    >>= \count -> addFromConsole count      -- Start recursive adder
+    >>= \output ->                          -- Get output from IO
+        putStrLn "Your Sum: "               -- Make output look good
+        >> print output                     -- Print final output
 
+-- getNumberFromConsole prints a header followed by reading an int from the console
 getNumberFromConsole :: IO Int
-getNumberFromConsole =
-    putStrLn "Enter Number: "
-    >> readInt
+getNumberFromConsole = 
+    putStrLn "Enter Number: "               -- Print "Enter Number: "
+    >> readInt                              -- Read number from console
 
+-- addFromConsole recursively adds integers from the console
 addFromConsole :: Int -> IO Int
-addFromConsole 0 = return 0
-addFromConsole n =
-  addFromConsole (n - 1)
-    >>= \recurs ->
-        getNumberFromConsole
-            >>= \input -> return (input + recurs)
+addFromConsole 0 = return 0             -- Base Case
+addFromConsole n =                      
+  addFromConsole (n - 1)                -- Recursively call addFromConsole, decrementing our remaining count
+    >>= \recurs ->                      -- Get output from recursive call
+        getNumberFromConsole            -- Get the next number to add from the console
+            >>= \input -> return (input + recurs)   -- Add the recursive output and the input from the console
 
 
+-- readInt gets a line from the console and reads it as an int
 readInt :: IO Int
-readInt = readLn
+readInt = readLn    -- Get line fron console
